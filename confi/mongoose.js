f94e12f88825668a0dee0f626f9c61e84a21257b
Narinder Kumar <narinder111198@gmail.com>
2023-01-17 23:57:53 +0530

    Formatting changes

--- confi/mongoose.js
+++ confi/mongoose.js
@@ -3,16 +3,17 @@
 const mongoose=require ('mongoose');
 
 // to connect to the database
-mongoose.connect("mongodb://localhost/todo_app");
+mongoose.connect("mongodb://localhost/todo_app", {useNewUrlParser: true, useUnifiedTopology: true, });
 
 // to require the connection and check if it is successful
 const db =mongoose.connection;
-
-//to see if there is an error
-db.on('error',console.error.bind(console,"error in connecting to the the database"));
 
 // if up and running and to print the message
 db.once('open',function()
 {
     console.log('connected to the database::MongoDB');
 });
+
+//to see if there is an error
+db.on('error',console.error.bind(console,"error in connecting to the the database"));
+
